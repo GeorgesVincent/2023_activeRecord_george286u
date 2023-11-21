7fb9c22a458aaf13030119c5e817edf2033ed964
GEORGES Vincent <vincent.georges3@etu.univ-lorraine.fr>
2023-11-21 15:50:54 +0100

    supression import inutiles

--- testpersonne/src/PrincipaleJDBC.java
+++ testpersonne/src/PrincipaleJDBC.java
@@ -1,10 +1,8 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Properties;
 
 public class PrincipaleJDBC {
 
